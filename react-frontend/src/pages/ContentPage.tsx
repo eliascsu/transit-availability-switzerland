604431a0a72033fdbfe810fb4aff6af23062d343
Elias Csuka <58990354+neonfighter28@users.noreply.github.com>
2023-11-17 17:20:13 +0100

    display base map

--- react-frontend/src/pages/ContentPage.tsx
+++ react-frontend/src/pages/ContentPage.tsx
@@ -1,50 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, useMap, WMSTileLayer } from 'react-leaflet'
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import './pages.css';
+
 //import { Loader } from '@googlemaps/js-api-loader';
-
-
-
 function ContentPage() {
-    //MapProvider();
     return (
-        <div>
-            <h1>Content Page</h1>
-            <div id="map" style={{ height: "800px" }}></div>
-        </div>
+        <MapContainer className="map-container" center={[47.36, 8.53]} zoom={10} scrollWheelZoom={false}>
+            <WMSTileLayer url="https://wms.geo.admin.ch/services/service?" layers="ch.swisstopo.pixelkarte-farbe-pk100.noscale" format="image/png" crs={L.CRS.EPSG4326}/>
+        </MapContainer>
     );
 }
-/*
-async function MapProvider() {
-    const loader = new Loader({
-        apiKey: "",
-        version: "weekly",
-    });
 
-    loader.load().then(async () => {
-        const map = new google.maps.Map(
-            document.getElementById("map") as HTMLElement,
-            
-            {
-                zoom: 13,
-                center: { lat: 47.37333184, lng: 8.53749785 },
-            }
-            ) 
-        
-        // Now i want to add the file mygeodata_merged.json to the map var
-
-        // Add GeoJSON
-        // Read the file
-        const transitLayer = new google.maps.TransitLayer();
-        const mygeodata_merged =await getGeoJson()
-        map.data.addGeoJson(mygeodata_merged);
-
-        transitLayer.setMap(map);
-        console.log(mygeodata_merged)})
-};
-
-async function getGeoJson() {
-    const mygeodata_merged = await fetch("/api/density_map").then((res) => res.json());
-    console.log(mygeodata_merged)
-    return mygeodata_merged;
-}
-*/
 export default ContentPage;
