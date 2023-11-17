7235bba6aaafe92f1a83f180bf445e85f0436612
Elias Csuka <58990354+neonfighter28@users.noreply.github.com>
2023-11-17 17:42:11 +0100

    add pop density & PT quality layer

--- react-frontend/src/pages/ContentPage.tsx
+++ react-frontend/src/pages/ContentPage.tsx
@@ -6,8 +6,10 @@ import './pages.css';
 //import { Loader } from '@googlemaps/js-api-loader';
 function ContentPage() {
     return (
-        <MapContainer className="map-container" center={[47.36, 8.53]} zoom={10} scrollWheelZoom={false}>
+        <MapContainer className="map-container" center={[47.36, 8.53]} zoom={10} scrollWheelZoom={true}>
             <WMSTileLayer url="https://wms.geo.admin.ch/services/service?" layers="ch.swisstopo.pixelkarte-farbe-pk100.noscale" format="image/png" crs={L.CRS.EPSG4326}/>
+            <WMSTileLayer url="https://wms.geo.admin.ch/services/service?" layers="ch.are.erreichbarkeit-oev" format="image/png" crs={L.CRS.EPSG4326} transparent={true} opacity={0.5}/>
+            <WMSTileLayer url="https://wms.geo.admin.ch/services/service?" layers="ch.bfs.volkszaehlung-bevoelkerungsstatistik_einwohner" format="image/png" crs={L.CRS.EPSG4326} transparent={true} opacity={0.5}/>
         </MapContainer>
     );
 }
