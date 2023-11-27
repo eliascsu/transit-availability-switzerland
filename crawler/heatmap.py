1847a35bcd78d849c65a4e435aaf3ab7b285f12b
Elias Csuka <58990354+neonfighter28@users.noreply.github.com>
2023-11-27 10:41:22 +0100

    vastly improve speed by using vectorization

--- crawler/heatmap.py
+++ crawler/heatmap.py
@@ -3,6 +3,7 @@ import requests
 from zipfile import ZipFile
 import pyproj
 from pyproj import CRS, transform
+import os
 import warnings
 LV95 = CRS.from_epsg(2056)
 WGS84 = CRS.from_epsg(4326)
@@ -15,42 +16,47 @@ TMP_PATH = "./tmp/"
 FNAME = "population"
 CSV_NAME = "OeV_Haltestellen_ARE"
 
-r = requests.get(URL)
+def download_and_extract(extract_to):
+    response = requests.get(URL)
+    if response.status_code == 200:
+        zip_path = os.path.join(extract_to, FNAME + ".zip")
+        with open(zip_path, 'wb+') as file:
+            file.write(response.content)
 
-with open(TMP_PATH + FNAME + ".zip", "wb+") as f:
-    f.write(r.content)
+        with ZipFile(zip_path, 'r') as zipObj:
+            zipObj.extractall(extract_to)
+    else:
+        raise Exception(f"Failed to download file: Status code {response.status_code}")
 
-with ZipFile(TMP_PATH + FNAME + ".zip", "r") as zipObj:
-    zipObj.extractall(TMP_PATH + FNAME + "/")
-
-# /tmp/population/ag-b-00.03-vz2022statpop/STATPOP2022.csv
-with open(TMP_PATH + FNAME + "/ag-b-00.03-vz2022statpop/" + "STATPOP2022" + ".csv", "r") as f:
-    df = pd.read_csv(f, sep=";")
-    lat = df["E_KOORD"]
-    lng = df["N_KOORD"]
-    pop = df["B22BTOT"]
-    # Simplify df
-    new_df = pd.DataFrame({"lat": lat, "lng": lng, "pop": pop})
-
-    # Remove max two values of df without changing new_df
-    new_df = new_df.drop(new_df["pop"].idxmax())
-    new_df = new_df.drop(new_df["pop"].idxmax())
-
-    max_pop = new_df["pop"].max()
+def transform_coordinates(df):
+    """Transform coordinates from LV95 to WGS84."""
     transformer = pyproj.Transformer.from_crs(LV95, WGS84, always_xy=True)
-    
-    for i, (longitude, latitude, population) in enumerate(zip(new_df["lat"], new_df["lng"], new_df["pop"])):
-        if i % 10000 == 0:
-            print(f"{i} of {len(lat.index)} done.")
-        new_lat, new_lng = transformer.transform(longitude, latitude)
-        new_df["lng"].loc[i] = new_lat
-        new_df["lat"].loc[i] = new_lng
-        # new_df["pop"].loc[i] = population / max_pop # Linear calculation
+    transformed_coords = df.apply(lambda row: transformer.transform(row['E_KOORD'], row['N_KOORD']), axis=1)
+    df[['lng', 'lat']] = pd.DataFrame(transformed_coords.tolist(), index=df.index)
+    return df
 
-        new_df["pop"].loc[i] = ((population / 20) ** 2) + 0.5 if ((population / 20) ** 2) + 0.5 < 40 else 40
-        
-    # Write to csv
-    # drop two last rows
-    new_df = new_df.drop(new_df.tail(2).index)
 
-    new_df.to_csv(TMP_PATH + FNAME + "-updated.csv", index=False)
+def process_data():
+    """Main function to process the data."""
+    csv_path = os.path.join(TMP_PATH, FNAME, "ag-b-00.03-vz2022statpop", "STATPOP2022.csv")
+    df = pd.read_csv(csv_path, sep=";")
+
+    # Drop unused columns and normalize population
+    df = df.drop(columns=["RELI", "B22B11"])
+    # Normalize population using this formula
+    # ((population / 20) ** 2) + 0.5 if ((population / 20) ** 2) + 0.5 < 40 else 40
+    df['pop'] = ((df['B22BTOT'] / 20) ** 2) + 0.5
+    df.loc[df['pop'] > 40, 'pop'] = 40
+
+    # Transform coordinates
+    df = transform_coordinates(df[['E_KOORD', 'N_KOORD', 'pop']])
+    df = df.drop(columns=['E_KOORD', 'N_KOORD'])
+    new_order = ['lat', 'lng', 'pop']
+    df = df.reindex(columns=new_order)
+    df.rename(columns={'pop': 'intensity'}, inplace=True)
+    # Write to CSV
+    df.to_csv(os.path.join(TMP_PATH, FNAME + "-updated.csv"), index=False)
+
+if (__name__ == "__main__"):
+    download_and_extract(TMP_PATH)
+    process_data()
