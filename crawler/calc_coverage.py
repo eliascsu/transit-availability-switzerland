3586e9cbd09155289f9fa92f534bd13f7a29541e
neonfighter28 <58990354+neonfighter28@users.noreply.github.com>
2023-11-28 22:31:26 +0100

    reformatting

--- crawler/calc_coverage.py
+++ crawler/calc_coverage.py
@@ -3,47 +3,58 @@ import json
 import pandas as pd
 from utils import haversine_vectorized
 from constants import data_root
+import time
 
-population = pd.read_csv(os.path.join(data_root, "dataset_population.csv"))
+time_start = time.time()
 
-@staticmethod
-def calculate_population_served_per_coordinate(x, y, kat):
-    global population
-    global points_not_served
+pop = pd.read_csv(os.path.join(data_root, "dataset_population.csv"))
+points_not_served = pd.DataFrame()
 
+def calculate_population_served_per_coordinate(x, y, kat, population):
     # Calculate Distance and Filter Points
-    distances = haversine_vectorized(x, y, 
-                                            population["lng"], 
-                                            population["lat"], 
+    if kat == 0:
+        return population, 0
+    distances = haversine_vectorized(x, y,
+                                            population["lng"],
+                                            population["lat"],
                                             )
     # Remove distances from population array
-    if (kat == 1 or kat == 2):
+    if kat in (1, 2):
         res = population[distances <= 1000]["pop_actual"].sum()
         population = population[distances > 1000]
-    elif (kat == 3):
+    elif kat == 3:
         res = population[distances <= 750]["pop_actual"].sum()
         population = population[distances > 750]
-    elif (kat == 4):
+    elif kat == 4:
         res = population[distances <= 500]["pop_actual"].sum()
         population = population[distances > 500]
-    elif (kat == 5):
+    elif kat == 5:
         res = population[distances <= 300]["pop_actual"].sum()
         population = population[distances > 250]
-    elif (kat == 0): res = 0
-    else: raise Exception(f"Invalid Kat {kat}")
 
-    points_not_served = population
-    return res
+    else: res = 0
+
+    return population, res
 
 with open(os.path.join(data_root, "dataset_OeV_Haltestellen_ARE.geojson")) as f:
     public_transport = json.load(f)
 
-total_population = population["pop_actual"].sum()
+total_population = pop["pop_actual"].sum()
 sum = 0
 
 for i, feature in enumerate(public_transport["features"]):
     if i % 100 == 0:
-        print("Progress: ", i, "/", len(public_transport["features"]), "Population Served: ", sum / total_population, "Population: ", sum)
-    sum += calculate_population_served_per_coordinate(feature["geometry"]["coordinates"][0], feature["geometry"]["coordinates"][1] , feature["properties"]["Hst_Kat"])
+        print(
+            "Progress: ", i, "/", len(public_transport["features"]),
+            "Population Served: ", sum / total_population, "Population: ", sum)
+    pop, s = calculate_population_served_per_coordinate(
+        feature["geometry"]["coordinates"][0],
+        feature["geometry"]["coordinates"][1] ,
+        feature["properties"]["Hst_Kat"], pop
+        )
+    sum += s
 
-points_not_served.to_csv(os.path.join(data_root, "dataset_unserved.csv"), index=False,)
+pop.to_csv(os.path.join(data_root, "dataset_unserved.csv"), index=False,)
+
+time_end = time.time()
+print("Time elapsed: ", time_end - time_start)
