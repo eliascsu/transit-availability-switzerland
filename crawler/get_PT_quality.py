ce7a2858ce591fb69fd71c1ba4f4afe2e2edd5ae
Elias Csuka <58990354+neonfighter28@users.noreply.github.com>
2023-11-29 17:25:57 +0100

    refactor to use main method

--- crawler/get_PT_quality.py
+++ crawler/get_PT_quality.py
@@ -3,12 +3,16 @@ from zipfile import ZipFile
 import requests
 from csvtogjson import create_geojson
 
-DOWNLOAD_URL = "https://data.geo.admin.ch/ch.are.gueteklassen_oev/gueteklassen_oev_2023/gueteklassen_oev_2023_2056.gpkg.zip"
-SAVE_PATH = "./backend/data/"
+def main():
+    DOWNLOAD_URL = "https://data.geo.admin.ch/ch.are.gueteklassen_oev/gueteklassen_oev_2023/gueteklassen_oev_2023_2056.gpkg.zip"
+    SAVE_PATH = "./backend/data/"
 
-CSV_NAME = "OeV_Haltestellen_ARE"
-r = requests.get(DOWNLOAD_URL)
+    CSV_NAME = "OeV_Haltestellen_ARE"
+    r = requests.get(DOWNLOAD_URL)
 
-archive = ZipFile(BytesIO(r.content))
-csv = archive.open(CSV_NAME + ".csv")
-create_geojson(CSV_NAME + ".csv", csv)
+    archive = ZipFile(BytesIO(r.content))
+    csv = archive.open(CSV_NAME + ".csv")
+    create_geojson(CSV_NAME + ".csv", csv)
+
+if __name__ == "__main__":
+    main()
