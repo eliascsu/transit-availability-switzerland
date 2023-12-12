4f6c35d76b33a2a9fc4eb96d4322d9f7e024a40f
Elias Csuka <58990354+neonfighter28@users.noreply.github.com>
2023-12-12 11:08:46 +0100

    meow

--- react-frontend/src/pages/components/maps/PtMap.tsx
+++ react-frontend/src/pages/components/maps/PtMap.tsx
@@ -1,14 +1,20 @@
-import { TileLayer, WMSTileLayer, useMap } from "react-leaflet";
+import { TileLayer, WMSTileLayer, useMap, useMapEvents } from "react-leaflet";
 import { MapContainer } from "react-leaflet";
 
 import { getPTData } from "../../../router/resources/data";
 import { makePTCirclesFromData } from "../../utils/qual_layers";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSwissTopoContext } from "../../ctx/Swisstopo";
+import proj4 from "proj4";
+
+const wgs84 = "EPSG:4326"
+const lv95 = '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs'
+
 
 export default function PtMap() {
     const layerStorage = useRef<L.GeoJSON<any, any>[]>([]);
     const layers = useRef<L.GeoJSON<any, any>[]>([]);
+    const [infoPtStop, setInfoPtStop] = useState<string>("")
     const {useSwissTopoMap} = useSwissTopoContext()
 
     useEffect(() => {
@@ -18,6 +24,37 @@ export default function PtMap() {
             }
         })
     }, []);
+
+    function InfoBox() {
+        if (infoPtStop == "") {
+            return null;
+        }
+        return (
+            <div dangerouslySetInnerHTML={{__html: infoPtStop}}>
+            </div>
+        )
+    }
+
+    function MapEvents() {
+        const map = useMapEvents(
+            {
+            click: (e) => {
+            const [x, y] = proj4(wgs84, lv95, [e.latlng.lng, e.latlng.lat]);
+            const url_ident = "https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry="+x+","+ y + "&imageDisplay=400,400,96&mapExtent="+ (x-4000)+ "," + (y-4000) + ","+ (x+4000) + "," + (y+4000) +"&geometryFormat=geojson&geometryType=esriGeometryPoint&lang=en&layers=all:ch.bav.haltestellen-oev&limit=10&returnGeometry=true&sr=2056&timeInstant=2021&tolerance=10"
+            fetch(url_ident).then(response => response.json()).then(data => {
+                if (data.results[0].id != undefined)
+                console.log(data.results[0].id)
+                return data.results[0].id;
+            }
+            ).then(id => {
+            const url = "https://api3.geo.admin.ch/rest/services/ech/MapServer/ch.bav.haltestellen-oev/"+id+"/htmlPopup?coord=" + x + "," + y + "&lang=en&tolerance=0&sr=2056"
+            fetch(url).then(response => response.text()).then(data => {
+                setInfoPtStop(data);
+            })})
+        }}
+        )
+        return null;
+    }
 
     function MakePtMap() {
         const map = useMap();
@@ -46,7 +83,9 @@ export default function PtMap() {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <WMSTileLayer url="https://wms.geo.admin.ch/" layers="ch.are.gueteklassen_oev" format="image/png" transparent={true} opacity={0.5} />
+                <MapEvents/>
             </MapContainer>
+            <InfoBox/>
             <Legend/>
             </div>
         )
@@ -59,7 +98,9 @@ export default function PtMap() {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <MakePtMap/>
+                <MapEvents/>
             </MapContainer>
+            <InfoBox/>
             <Legend/>
         </div>
     )
