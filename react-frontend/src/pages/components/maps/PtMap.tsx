3d9ca7809e01d1be2de06b446696f2e1fc6a576f
Cyborgcedy <cedric.t.koller@gmail.com>
2023-12-15 11:40:43 +0100

    layout and color update

--- react-frontend/src/pages/components/maps/PtMap.tsx
+++ react-frontend/src/pages/components/maps/PtMap.tsx
@@ -136,38 +136,35 @@ function Legend() {
             <h2>
                 This map represents the connection quality of public transit in Switzerland
                 </h2>
-                <ul>
-                    <li>
+                <ul style={{ listStyleType: "none" }}>
+                    <p className="listElement">
                         <div id="rectangle" style={{
                             width: "20px",
                             height:"15px",
                             background:"#700038"}}>
                         </div><p>Very good connection quality</p>
-                    </li>
-                    <li>
+                    </p>
+                    <p className="listElement">
                         <div id="rectangle" style={{
                             width: "20px",
                             height:"15px",
                             background:"#9966FF"}}>
-                        </div>
-                    <p>Good connection quality</p>
-                    </li>
-                    <li>
+                        </div><p>Good connection quality</p>
+                    </p>
+                    <p className="listElement">
                         <div id="rectangle" style={{
                             width: "20px",
                             height:"15px",
                             background:"#00B000"}}>
-                        </div>
-                    <p>Medium connection quality</p>
-                    </li>
-                    <li>
+                        </div><p>Medium connection quality</p>
+                    </p>
+                    <p className="listElement">
                         <div id="rectangle" style={{
                             width: "20px",
                             height:"15px",
                             background:"#B3FF40"}}>
-                        </div>
-                    <p>Bad connection quality</p>
-                    </li>
+                        </div><p>Bad connection quality</p>
+                    </p>
                 </ul>
         </div>
     )
