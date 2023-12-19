cc1215a593961b7b65db51cb427166e392f00ced
Joshua Durrant <jdurrant@student.ethz.ch>
2023-12-19 10:46:06 +0100

    Landing page update, image has to be rescaled

--- react-frontend/src/pages/LandingPage.tsx
+++ react-frontend/src/pages/LandingPage.tsx
@@ -2,47 +2,48 @@ import { Layout, Col, Row, Button } from 'antd';
 import { Link } from 'react-router-dom';
 import "./pages.css";
 
-const {Content, Footer} = Layout;
+const {Content} = Layout;
 
 function LandingPage() {
     return (
-        <Layout className="layout" id="landingPage">
-            <div className="layer background">
-                <img src="metro_map.svg" alt='Custom metro map used in the background'/>
-            </div>
-            <div id='leftGreen'/>
-            <div className='layer front'>
-                <Content className="content">
-                    <Row>
-                        <Col span={6}>
-                            <div>
-                                <span id="topic">
+        <Layout className="layout">
+                <Content>
+                    <div className='img_wrapper'>
+                        <img className="background_img" src="metro_map.svg" alt=""/>
+                        <Row>
+                            <Col span={8} id="col8">
+                                <div className="label">
                                     <h5>FWE 2023</h5>
-                                </span>
-                            </div>
-                        </Col>
-                        <Col span={18}>
-                            <div id="titelDiv">
-                                <h1 id="title">Open Data Map</h1>
-                                <Link id="contentLink" to="/content">
-                                    <Button id="startButton">Start</Button>
-                                </Link>
-                                <Link id="contentLink" to="/attributions">
-                                    <Button id="attributionsButton" type="text">Attributions</Button>
-                                </Link>
-                            </div>
-                        </Col>
-                    </Row>
+                                </div>
+                            </Col>
+                            <Col span={16}/>
+                        </Row>
+                        <Row>
+                            <Col span={4} className='col4'/>
+                            <Col span={16} className='col16'>
+                                <div id="titelDiv">
+                                    <h1 id="title">Open Data Map</h1>
+                                    <Link id="contentLink" to="/content">
+                                        <Button id="startButton">Start</Button>
+                                    </Link>
+                                    <Link id="contentLink" to="/attributions">
+                                        <Button id="attributionsButton" type="text">Attributions</Button>
+                                    </Link>
+                                </div>
+                            </Col>
+                            <Col span={4} className='col4'/>
+                        </Row>
+                    </div>
                 </Content>
-            </div>
-            <Footer className="footer" id="footerLanding">
-                <span id="footerText">
-                    <h5 id="credits">Elias Csuka, Joshua Durrant, Leander Hemmi, Cedric Koller, Mathias Schmid</h5>
-                </span>
-            </Footer>
-
         </Layout>
     );
 }
 
 export default LandingPage;
+
+/* 
+<div className="layer background">
+                <img src="metro_map.svg" alt='Custom metro map used in the background'/>
+            </div>
+            <div id='leftGreen'/>
+*/
