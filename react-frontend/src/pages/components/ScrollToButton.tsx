be94c0d85c738226643f312f6802d25281a30010
Joshua Durrant <jdurrant@student.ethz.ch>
2023-12-21 12:06:49 +0100

    Finished light-mode and QoL changes

--- react-frontend/src/pages/components/ScrollToButton.tsx
+++ react-frontend/src/pages/components/ScrollToButton.tsx
@@ -1,6 +1,8 @@
 import double_down_chevron from "../../svg/double_down_chevron.svg";
 import double_up_chevron from "../../svg/double_up_chevron.svg";
 
+import "../css/bundle.css";
+
 export function ScrollToBottom() {
     const scrollToBottom = () => {
         window.scrollTo({
@@ -16,15 +18,27 @@ export function ScrollToBottom() {
 }
 
 export function ScrollToTop() {
+    let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+    let isDarkMode = mediaQueryObj.matches;
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: "smooth",
         });
     };
-    return (
-        <div className="scroll-to-top" onClick={scrollToTop}>
-            <img src={double_up_chevron}></img>
-        </div>
-    );
+
+    if (isDarkMode) {
+        return (
+            <div className="scroll-to-top" onClick={scrollToTop}>
+                <img src={double_up_chevron}></img>
+            </div>
+        );
+    } else {
+        return (
+            <div className="scroll-to-top invert-img" onClick={scrollToTop}>
+                <img src={double_up_chevron}></img>
+            </div>
+        );
+    }
 }
